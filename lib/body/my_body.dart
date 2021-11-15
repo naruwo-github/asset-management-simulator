da7fb8a916d655d63c1d7eb0bca0c4e42892f6cb
野川成己 <naruo0711@ab.auone-net.jp>
2021-11-16 06:39:58 +0900

    Show disclaimer text by dialog driving by tapping.

--- lib/body/my_body.dart
+++ lib/body/my_body.dart
@@ -2,6 +2,7 @@ import 'package:asset_management_simulator/body/chart/chart.dart';
 import 'package:asset_management_simulator/body/resultText/result_text.dart';
 import 'package:asset_management_simulator/body/setting/setting.dart';
 import 'package:asset_management_simulator/services/admob.dart';
+import 'package:asset_management_simulator/services/string_manager.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
@@ -46,6 +47,23 @@ class MyBody {
                     _setSavingPeriod,
                   ),
                   ResultText().getWidget(_calculatedResult, _context),
+                  OutlinedButton(
+                    onPressed: () {
+                      showDialog(
+                          context: _context,
+                          builder: (context) {
+                            return AlertDialog(
+                              content: Text(StringManager().disclaimerContent),
+                            );
+                          });
+                    },
+                    child: Text(StringManager().disclaimerTitle),
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.black,
+                      shape: const StadiumBorder(),
+                      side: const BorderSide(color: Colors.green),
+                    ),
+                  ),
                 ],
               ),
             ),

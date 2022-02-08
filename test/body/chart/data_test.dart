2db812938cd4d62b06ffc4f359b91c20a35a9189
野川成己 <naruo0711@ab.auone-net.jp>
2022-02-08 14:53:22 +0900

    Create files for the tests.

--- test/body/chart/data_test.dart
+++ test/body/chart/data_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('sample unit test', () {
+  test('ChartData test', () {
     const answer = 1;
     expect(answer, 1);
   });
---
+++ test/body/resultText/result_text_test.dart
@@ -0,0 +1,1 @@
+// There are no unit tests.
